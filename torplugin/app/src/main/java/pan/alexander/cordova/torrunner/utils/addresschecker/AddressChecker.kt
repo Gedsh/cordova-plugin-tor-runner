dbfeae15a5c45bd967473751ab05e12283ffc94f
Oleksandr Garmatin <invizible.soft@gmail.com>
2025-07-28 12:02:03 +0300

    feat: verify the certificate domain

--- torplugin/app/src/main/java/pan/alexander/cordova/torrunner/utils/addresschecker/AddressChecker.kt
+++ torplugin/app/src/main/java/pan/alexander/cordova/torrunner/utils/addresschecker/AddressChecker.kt
@@ -1,9 +1,12 @@
 package pan.alexander.cordova.torrunner.utils.addresschecker
 
 import android.annotation.SuppressLint
+import pan.alexander.cordova.torrunner.utils.logger.Logger.logw
 import java.net.InetSocketAddress
 import java.net.SocketTimeoutException
+import java.security.cert.X509Certificate
 import javax.inject.Inject
+import javax.net.ssl.HttpsURLConnection
 import javax.net.ssl.SSLContext
 import javax.net.ssl.SSLSocket
 import javax.net.ssl.SSLSocketFactory
@@ -54,6 +57,10 @@ class AddressChecker @Inject constructor() {
                 socket.enabledProtocols = socket.supportedProtocols
                 socket.startHandshake()
 
+                if (!validateCertificateDomain(socket, domain)) {
+                    return@use false
+                }
+
                 val writer = socket.outputStream.bufferedWriter()
                 writer.write("HEAD / HTTP/1.1\r\nHost: $domain\r\nConnection: close\r\n\r\n")
                 writer.flush()
@@ -61,12 +68,38 @@ class AddressChecker @Inject constructor() {
                 val reader = socket.inputStream.bufferedReader()
                 val responseLine = reader.readLine()
 
+                if (!responseLine.startsWith("HTTP")) {
+                    logw("Address ${domain}:${port} respond with $responseLine")
+                }
+
                 responseLine != null && responseLine.startsWith("HTTP")
             }
         } catch (_: SocketTimeoutException) {
+            logw("Address ${domain}:${port} timeout")
             false
-        } catch (_: Exception) {
+        } catch (e: Exception) {
+            if (e.message != null) {
+                if (e.cause != null) {
+                    logw("Address ${domain}:${port} unreachable. Reason: ${e.javaClass.name} Details: ${e.message} ${e.cause}")
+                } else {
+                    logw("Address ${domain}:${port} unreachable. Reason: ${e.javaClass.name} Details: ${e.message}")
+                }
+            } else {
+                logw("Address ${domain}:${port} unreachable. Reason: ${e.javaClass.name} ")
+            }
+
             false
         }
+
+    private fun validateCertificateDomain(socket: SSLSocket, domain: String): Boolean {
+        val session = socket.session
+        val hostnameVerifier = HttpsURLConnection.getDefaultHostnameVerifier()
+        if (!hostnameVerifier.verify(domain, session)) {
+            val cert = session.peerCertificates?.firstOrNull() as? X509Certificate
+            logw("Address $domain certificate failure, mismatch domain in SAN or CN: ${cert?.subjectX500Principal?.name}")
+            return false
+        }
+        return true
+    }
 
 }
